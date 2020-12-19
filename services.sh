a5757bb1022ae99eba0439006677f3227eba449d
Toby Petty <tobypetty1@gmail.com>
2020-12-19 00:04:45 +0000

    Moved to rofi -dmenu (in nix compatible way, need to fix this)

--- services.sh
+++ services.sh
@@ -1,8 +1,8 @@
-selected=$(systemctl list-unit-files --no-pager --type=service --no-legend | dmenu | awk '{print $1;}')
+selected=$(systemctl list-unit-files --no-pager --type=service --no-legend | env --unset=LD_PRELOAD LOCALE_ARCHIVE=$(env --unset=LD_PRELOAD nix-build '<nixpkgs>' -A glibcLocales)/lib/locale/locale-archive rofi -dmenu | awk '{print $1;}')
 
 selected=$(echo $selected | awk '{print $1;}')
 
-action=$(echo -e "start\nstop\nrestart" | dmenu)
+action=$(echo -e "start\nstop\nrestart" | env --unset=LD_PRELOAD LOCALE_ARCHIVE=$(env --unset=LD_PRELOAD nix-build '<nixpkgs>' -A glibcLocales)/lib/locale/locale-archive rofi -dmenu)
 
 case "$action" in
     "start")
